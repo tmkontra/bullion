656fb953759bc42e35b36e42deea8910fa768c9e
Tyler M. Kontra <tyler@tylerkontra.com>
2021-07-25 11:28:22 -0700

    public interface to core is supervisor

--- bullion-core/lib/bullion_core/table_supervisor.ex
+++ bullion-core/lib/bullion_core/table_supervisor.ex
@@ -11,17 +11,56 @@ defmodule BullionCore.TableSupervisor do
     Supervisor.init([TableServer], strategy: :simple_one_for_one)
   end
 
+  defp via(table_id) do
+    TableServer.via(table_id)
+  end
+
   def start_table({_table_name, _buyin_chips, _buyin_dollars} = args) do
     Supervisor.start_child(__MODULE__, [args])
+  end
+
+  defp table_process_exists?(table) do
+    case Registry.lookup(Registry.Table, table) do
+      [] -> false
+      _ -> true
+    end
   end
 
   def stop_table(table_id) do
     Supervisor.terminate_child(__MODULE__, table_pid(table_id))
   end
 
+  def view_table(table_id) when is_binary(table_id) do
+    table_id
+    |> via()
+    |> TableServer.view_table()
+  end
+
+  def view_table(table_pid) when is_pid(table_pid) do
+    TableServer.view_table(table_pid)
+  end
+
+  def add_player(table_id, player_name) do
+    table_id
+    |> via()
+    |> TableServer.add_player(player_name)
+  end
+
+  def player_buyin(table_id, player_id) do
+    table_id
+    |> via()
+    |> TableServer.player_buyin(player_id)
+  end
+
+  def player_cashout(table_id, player_id, chip_count) do
+    table_id
+    |> via()
+    |> TableServer.player_cashout(player_id, chip_count)
+  end
+
   defp table_pid(table_id) do
     table_id
-    |> TableServer.via()
+    |> via()
     |> GenServer.whereis()
   end
 
--- bullion/lib/bullion_web/controllers/v2_controller.ex
+++ bullion/lib/bullion_web/controllers/v2_controller.ex
@@ -10,51 +10,48 @@ defmodule BullionWeb.V2Controller do
   end
 
   def start_game(conn, %{"name" => name, "buyin_chips" => buyin_chips, "buyin_dollars" => buyin_dollars}) do
-    {buyin_chips, _err} = Integer.parse(buyin_chips)
-    {buyin_dollars, _err} = Integer.parse(buyin_dollars)
-    {:ok, pid} = TableSupervisor.start_table({name, buyin_chips, buyin_dollars})
-    {:ok, table} = pid |> TableServer.view_table()
-    table = TableV2.save_new_table(table)
-    conn
-    |> redirect(to: Routes.v2_path(conn, :view_game, table.id))
+    with {buyin_chips, _err} = Integer.parse(buyin_chips),
+         {buyin_dollars, _err} = Integer.parse(buyin_dollars),
+         {:ok, pid} = TableSupervisor.start_table({name, buyin_chips, buyin_dollars}),
+         {:ok, table} = TableSupervisor.view_table(pid),
+         table = TableV2.save_new_table(table)
+    do
+      conn
+      |> redirect(to: Routes.v2_path(conn, :view_game, table.id))
+    end
   end
 
   def view_game(conn, %{"game_id" => game_id}) do
-    {:ok, table} = TableServer.via(game_id)
-    |> TableServer.view_table()
+    {:ok, table} = TableSupervisor.view_table(game_id)
     conn
     |> render("view.html", table: table)
   end
 
   def add_player(conn, %{"game_id" => game_id, "player" => %{"name" => name}}) do
-    table = TableServer.via(game_id)
-    {:ok, _plid} = TableServer.add_player(table, name)
+    {:ok, _plid} = TableSupervisor.add_player(game_id, name)
     conn
     |> redirect(to: Routes.v2_path(conn, :view_game, game_id))
   end
 
-  @spec add_buyin(Plug.Conn.t(), map) :: Plug.Conn.t()
   def add_buyin(conn, %{"game_id" => game_id, "player_id" => player_id}) do
-    table = TableServer.via(game_id)
     {player_id, _err} = Integer.parse(player_id)
-    :ok = TableServer.player_buyin(table, player_id)
+    :ok = TableSupervisor.player_buyin(game_id, player_id)
     conn
     |> redirect(to: Routes.v2_path(conn, :view_game, game_id))
   end
 
   def cashout_form(conn, %{"game_id" => game_id, "player_id" => player_id}) do
-    {:ok, table} = TableServer.via(game_id) |> TableServer.view_table()
     {player_id, _err} = Integer.parse(player_id)
+    {:ok, table} = TableSupervisor.view_table(game_id)
     {:ok, player} = Table.get_player(table, player_id)
     conn
     |> render("cashout.html", table: table, player: player)
   end
 
   def cashout(conn, %{"game_id" => game_id, "player_id" => player_id, "chip_count" => chip_count}) do
-    table = TableServer.via(game_id)
     {player_id, _err} = Integer.parse(player_id)
     {chip_count, _err} = Integer.parse(chip_count)
-    :ok = TableServer.player_cashout(table, player_id, chip_count)
+    :ok = TableSupervisor.player_cashout(game_id, player_id, chip_count)
     conn
     |> redirect(to: Routes.v2_path(conn, :view_game, game_id))
   end
